7aecf674c602b32c7553d56c2e54a30e9c6165f9
Abhishek Yadav <103676667+Abhishek9503@users.noreply.github.com>
2023-06-26 17:48:50 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.22%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -10,10 +10,33 @@ public:
         return ans;
     }
     
+    int topDownSolve(int n, vector<int> &dp){
+        
+        if(n==1 || n==0){
+            return n;
+        }
+        
+        
+        // step 3: check  if ans already exits
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        
+        //step 2: replace ans with dp[n]
+        dp[n]= topDownSolve(n-1,dp)+ topDownSolve(n-2,dp);
+            return dp[n];
+       
+}
+    
     int fib(int n) {
         
-        int ans= recSolve(n);
-        return ans;
+        // int ans= recSolve(n);
+        // return ans;
         
+        //step1 create dp arry
+        
+        vector<int> dp(n+1, -1);
+        int ans= topDownSolve(n, dp);
+        return ans;
     }
 };
