92206bb3adf964d27c4d327c142fb61e57ae0d81
Abhishek Yadav <103676667+Abhishek9503@users.noreply.github.com>
2023-06-26 18:01:01 +0530

    Time: 4 ms (50.33%), Space: 6.1 MB (26.22%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -10,6 +10,7 @@ public:
         return ans;
     }
     
+    
     int topDownSolve(int n, vector<int> &dp){
         
         if(n==1 || n==0){
@@ -28,6 +29,31 @@ public:
        
 }
     
+    
+    int bttomUpSolve(int n){
+        //step 1:
+        vector<int>dp(n+1, -1);
+        
+        //step 2: ovserve base case in above solution
+        dp[0]=0;
+        
+        if(n==0){
+            return dp[0];
+        }
+        
+        
+        dp[1]=1;
+        
+        //step 3:
+        
+        for(int i =2;i<=n;i++)
+            {
+                dp[i]=dp[i-1]+ dp[i-2]; 
+            } 
+            return dp[n];
+    }
+    
+    
     int fib(int n) {
         
         // int ans= recSolve(n);
@@ -35,8 +61,10 @@ public:
         
         //step1 create dp arry
         
-        vector<int> dp(n+1, -1);
-        int ans= topDownSolve(n, dp);
-        return ans;
+        // vector<int> dp(n+1, -1);
+        // int ans= topDownSolve(n, dp);
+        // return ans;
+        
+        return bttomUpSolve( n);
     }
 };
