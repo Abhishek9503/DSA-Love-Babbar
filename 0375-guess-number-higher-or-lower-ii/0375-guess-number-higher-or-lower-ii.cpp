b9e31287c2ca392e8fc3b5fec25277073b6b3d54
Abhishek Yadav <103676667+Abhishek9503@users.noreply.github.com>
2023-07-09 06:55:37 +0530

    Time: 101 ms (77.73%), Space: 8.2 MB (76.97%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -27,15 +27,49 @@ public:
             ans= min(ans,i +max(solveUsingMem(start,i-1,dp), solveUsingMem(i+1,end,dp)));
         }
                      
-                return dp[start][end]= ans;
+  return dp[start][end]= ans;
     }
+    
+    
+    
+    
+    
+    int solveUsingTab( int n){
+        vector<vector<int>>dp(n+2 , vector<int>(n+2,0));
+        
+        for(int start=n;start>=1;start--){
+            for(int end =1; end<=n;end++)
+            {
+                if(start>=end)continue;
+            
+            else
+            {
+                
+                   int ans= INT_MAX;
+        
+        for(int i=start; i<=end; i++)
+          {
+            ans= min(ans,i +max(dp[start][i-1], dp[i+1][end]));
+           }
+
+                dp[start][end]=ans;
+            }
+               
+        }
+    }
+    return dp[1][n];
+    
+                     }
     
     int getMoneyAmount(int n) {
         // int ans= solveUsingRecursion(1,n);
 
         
-        vector<vector<int>> dp(n+1, vector<int>( n+1,-1));
-       int ans= solveUsingMem(1, n ,dp);
+//         vector<vector<int>> dp(n+1, vector<int>( n+1,-1));
+//        int ans= solveUsingMem(1, n ,dp);
+        
+        
+         int ans= solveUsingTab(n);
         
         return ans;
     }
