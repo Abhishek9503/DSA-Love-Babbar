f72931cb59e93c696337b989b20628923524abcb
Abhishek Yadav <103676667+Abhishek9503@users.noreply.github.com>
2023-09-22 18:53:59 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (6.71%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,62 +1,54 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> ans;
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int total_element=m*n;
+      vector<int>ans;
         
+            int m = matrix.size();
+            int n= matrix[0].size();
+        int total_element= m*n;
         
         int startingRow=0;
-        int endingCol=n-1;
+        int endingCol=n-1;;
         int endingRow=m-1;
         int startingCol=0;
         
         
         int count=0;
-        while(count<total_element)
-        {
-               //Print Starting Row
+        while(count< total_element){
             
-            for(int i=startingCol;i<=endingCol && count< total_element;i++)
-            {
+            //printiung staring row
+            for(int i =startingCol; i<= endingCol  && count< total_element; i++){
                 ans.push_back(matrix[startingRow][i]);
-                  count++;
+                 count++;
+
             }
             startingRow++;
             
             
-               //Print Ending Col
-            
-             for(int i=startingRow;i<=endingRow  && count< total_element;i++)
-            {
+            //printing ending col
+             for(int i =startingRow; i<= endingRow  && count< total_element; i++){
                 ans.push_back(matrix[i][endingCol]);
-                   count++;
+                  count++;
             }
             endingCol--;
             
-            
-               //Print Ending Row
-            
-                for(int i=endingCol;i>=startingCol  && count< total_element;i--)
-            {
+            //priting end row
+             for(int i =endingCol; i>= startingCol  && count< total_element; i--){
                 ans.push_back(matrix[endingRow][i]);
-                      count++;
+                  count++;
             }
             endingRow--;
             
-            
-               //Print Starting Col
-            
-               
-                for(int i=endingRow;i>=startingRow  && count< total_element;i--)
-            {
+              //priting start col
+             for(int i =endingRow; i>= startingRow  && count< total_element; i--){
                 ans.push_back(matrix[i][startingCol]);
-                    count++;
-                    
+                  count++;
             }
             startingCol++;
         }
         return ans;
     }
 };
+
+
+
